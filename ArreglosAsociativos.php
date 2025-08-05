643e37bdef3360dfd6c8c9ff939f1268a23baf89
Dieguuiyo <sabriidiiego@gmail.com>
2025-08-05 17:17:03 -0300

    trabajo arreglos asociativos

---
+++ ArreglosAsociativos.php
@@ -0,0 +1,13 @@
+<?php
+$persona1 = ['nombre' => 'Juan',
+              'apellido' => 'Perez',
+              'edad' => 19,
+              'nacimiento' => ['dia' > 1,
+                              'mes' > 1,
+                              'año' > 200]
+];
+$persona1['edad'];
+$persona1['nacimiento'] ['dia'];
+
+?>
+
