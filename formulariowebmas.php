f5d22c4aaa57c31f80c2d68c98d492ec5f50af9b
Alumno <alumno@isbo.edu.uy>
2025-08-12 17:59:39 -0300

    ahiva

---
+++ formulariowebmas.php
@@ -0,0 +1,16 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $nombre = ($_POST["nombre"]);
+    $edad = ($_POST[edad]);
+    $raza = ($_POST["raza"]);
+    $vacunas = ($_POST[vacunas]);
+    $FechaNac = ($_POST["FechaNac"]);
+
+
+    $mascota = [
+        
+    ];
+        
+};
+?>
+    
---
+++ jurado_digital_Diego_Escobar.php.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $relato = ($_POST["relato"]);
+$palabras = explode(" ", $relato);
+$contadorPalabras = count($palabras);
+$contadorCaracteres = strlen($relato);
+if ($contadorPalabras > 100) {
+        echo "el relato no puede tener mas de 100 palabras. Tiene $contadorPalabras palabras.";
+    } elseif ($contadorCaracteres > 700) {
+        echo "el relato no puede tener mas de 700 caracteres. Tiene $contadorCaracteres caracteres.";
+    } else {
+        echo "se recibio correctamente el relato.";
+    }
+}
+
+
+?>
